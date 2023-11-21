65c71f6a94402d6c61920b18bf95fb77de02758a
Halil YAŞ <106011017+halilyass@users.noreply.github.com>
2023-11-21 10:16:01 +0300

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Halil YAŞ on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
